38d3d2af7a8fbdc06e5b7d722a132d0b6a85e2fd
Bryce <bryce_fausto@yahoo.com>
2024-07-25 18:09:04 +0800

    Fixed UI of Create Subject Page

--- src/app/(dashboard)/subjects/create/page.tsx
+++ src/app/(dashboard)/subjects/create/page.tsx
@@ -1,25 +1,10 @@
+import FormLayout from '@/components/FormLayout'
 import CreateSubjectForm from '@/components/subjects/CreateSubjectForm'
-import { Button } from 'flowbite-react'
-import Link from 'next/link'
 
 export default function SubjectCreatePage() {
   return (
-    <div>
-      <div className="flex p-5">
-        <Link href="/subjects">
-          <Button className="justify-self-start">Back</Button>
-        </Link>
-        <div className="flex flex-auto">
-        </div>
-      </div>
-      <div className="flex justify-center">
-        <div className="flex flex-col">
-          <div className="mb-5">
-            <span className="text-4xl font-bold">Create Subject</span>
-          </div>
-          <CreateSubjectForm />
-        </div>
-      </div>
-    </div>
+    <FormLayout title="Create Subject" backUrl="/subjects">
+      <CreateSubjectForm />
+    </FormLayout>
   )
 }
