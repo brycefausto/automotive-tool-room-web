6368a318de1a54097042fd022b48de01cd12a3c5
Bryce <bryce_fausto@yahoo.com>
2026-08-01 07:50:34 +0800

    Add logs for TrialWrapper

--- src/components/trial/TrialWrapper.tsx
+++ src/components/trial/TrialWrapper.tsx
@@ -8,8 +8,10 @@ export default function TrialWrapper({
   children: React.ReactNode;
 }) {
   const trialStartDate = process.env.NEXT_PUBLIC_TRIAL_START_DATE || "";
+  console.log('Trial Start Date:', trialStartDate);
 
   const { isExpired } = getTrialStatus(trialStartDate);
+  console.log('Is Expired:', isExpired);
 
   if (isExpired) {
     return <TrialExpiredOverlay />
